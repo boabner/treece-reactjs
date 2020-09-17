92368a4d084f75dd4bb93aa39248ebabd4b39d4c
boabner <boabner@gmail.com>
2020-09-16 22:07:50 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,41 +9,52 @@ import Condicional from "./components/basicos/Condicional";
 import CondicionalComIf from "./components/basicos/CondicionalComIf";
 import Pai from "./components/comunicacao/direta/Pai";
 import Super from "./components/comunicacao/indireta/Super";
+import Input from "./components/form/Input";
+import Contador from "./components/contador/Contador";
 
 export default (props) =>
     <div className="App">
-        <Card titulo="#08 Comunicação Indireta" color="#000">
-            <Super>
-            </Super>
-        </Card>
-        <Card titulo="#07 Comunicação Direta" color="#29ABA4">
-            <Pai sobrenome="Freitas"></Pai>
-        </Card>
-        <Card titulo="#06 Condicional v2" color="#FA6900">
-            <CondicionalComIf numero={10}/>
-        </Card>
-        <Card titulo="#05 Condicionalv1" color="#E94C6F" >
-            <Condicional numero={11}/>
-        </Card>
-        <Card titulo="#04 Repetição" color="#008BBA">
-            <Repeticao/>
-        </Card>
-        <Card titulo="#03 Componente Com Filho" color="#FF85CB">
-            <ComFilhos>
-                <ul>
-                    <li>Ana</li>
-                    <li>Bia</li>
-                    <li>Carlos</li>
-                    <li>Daniel</li>
-                </ul>
-            </ComFilhos>
-        </Card>
-        <Card titulo="#02 Componente Com Parâmetro" color="#5BB12F">
-            <ComParametro titulo="Esse é o titulo"
-                          subtitulo="Esse é o subtitulo">
-            </ComParametro>
-        </Card>
-        <Card titulo="#01 Primeiro Componente" color="#9B539C">
-            <Primeiro/>
-        </Card>
+        <h1>Fundamentos React</h1>
+        <div className="Cards">
+            <Card titulo="#10  Contador" color="#ADC4CC">
+                <Contador passo={10} valor={100}/>
+            </Card>
+            <Card titulo="#09  Input" color="#DD5F32">
+                <Input />
+            </Card>
+            <Card titulo="#08  Comunicação Indireta" color="#000">
+                <Super>
+                </Super>
+            </Card>
+            <Card titulo="#07 Comunicação Direta" color="#29ABA4">
+                <Pai sobrenome="Freitas"/>
+            </Card>
+            <Card titulo="#06 Condicional v2" color="#FA6900">
+                <CondicionalComIf numero={10}/>
+            </Card>
+            <Card titulo="#05 Condicionalv1" color="#E94C6F" >
+                <Condicional numero={11}/>
+            </Card>
+            <Card titulo="#04 Repetição" color="#008BBA">
+                <Repeticao/>
+            </Card>
+            <Card titulo="#03 Componente Com Filho" color="#FF85CB">
+                <ComFilhos>
+                    <ul>
+                        <li>Ana</li>
+                        <li>Bia</li>
+                        <li>Carlos</li>
+                        <li>Daniel</li>
+                    </ul>
+                </ComFilhos>
+            </Card>
+            <Card titulo="#02 Componente Com Parâmetro" color="#5BB12F">
+                <ComParametro titulo="Esse é o titulo"
+                              subtitulo="Esse é o subtitulo">
+                </ComParametro>
+            </Card>
+            <Card titulo="#01 Primeiro Componente" color="#9B539C">
+                <Primeiro/>
+            </Card>
+        </div>
     </div>
---
+++ src/components/contador/Contador.jsx
@@ -0,0 +1,35 @@
+import React,  {Component} from "react";
+
+export default class Contador extends Component {
+
+    state = {
+        passo: this.props.passo || 1,
+        valor: this.props.passo || 0
+    }
+
+    inc = () => {
+        this.setState({
+            valor: this.state.valor + this.state.passo
+        })
+    }
+
+    dec = () => {
+        this.setState({
+            valor: this.state.valor - this.state.passo
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Contador</h2>
+                <h4>Passo: {this.state.passo}</h4>
+                <h4>Valor: {this.state.valor}</h4>
+                <div>
+                    <button onClick={this.inc}>+</button>
+                    <button onClick={this.dec}>-</button>
+                </div>
+            </div>
+        )
+    }
+}
